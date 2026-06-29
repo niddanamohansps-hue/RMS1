6545402bc1e5d733339c3741d3bbd633912a553b
Mohan Niddana <niddanamohansps@gmail.com>
2026-06-30 00:33:41 +0530

    Update settings.py
    later must be change allowed hosts

--- rms_backend/rms_backend/settings.py
+++ rms_backend/rms_backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
+ALLOWED_HOSTS = config("sps-rms-swxo.onrender.com", default="localhost,127.0.0.1", cast=Csv())
 
 # ─── Installed Apps ──────────────────────────────────────────────────────────
 INSTALLED_APPS = [
